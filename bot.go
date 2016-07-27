ec71e895598af85836bf615ab276491599445eaf
Grant Seltzer Richman <grantseltzer@gmail.com>
2016-07-26 20:46:50 -0400

    Setup main to consume shakebot library

--- bot.go
+++ bot.go
@@ -1,14 +1,31 @@
 package main
 
 import (
-	slackbot "github.com/BeepBoopHQ/go-slackbot"
+        "fmt"
+        "os"
+
+        "golang.org/x/net/context"
+
+        slackbot "github.com/BeepBoopHQ/go-slackbot"
+        "github.com/nlopes/slack"
 )
 
 func startShakebot() {
-	shakebot := slackbot.New("xoxb-63403884339-bezI1w68rxoJ0hOUclIWbvZr")
-	shakebot.Run()
+        shakebot := slackbot.New(os.Getenv("SLACK_API_TOKEN"))
+        shakebot.Hear("shakebot (.*)").MessageHandler(sonnetResponseHandler)
+        shakebot.Run()
+}
+
+func sonnetResponseHandler(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEvent) {
+        // evt.Msg.Text is the full string that was entered, so letsparse that through
+        // using the shakebot library, something like this:
+        // response := shakebot.GetQuote()
+        //      bot.Reply(evt, response, slackbot.WithTyping)
+
+        fmt.Println(evt.Msg.Text)
+        bot.Reply(evt, "SHAKESBOT QUOTE!", slackbot.WithTyping)
 }
 
 func main() {
-	startShakebot()
+        startShakebot()
 }
